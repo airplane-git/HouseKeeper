08ff23453c1378abefab6e4e30ec99ce97b5a5d6
123456 <123456>
2016-09-08 13:46:03 +0800

    做了个动画

--- app/src/main/java/cn/feicui/com/housekeeper/MainActivity.java
+++ app/src/main/java/cn/feicui/com/housekeeper/MainActivity.java
@@ -1,7 +1,13 @@
 package cn.feicui.com.housekeeper;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img_logo=(ImageView) findViewById(R.id.iv_logo);
+        AnimatorSet animator= (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.anim_logo);
+        animator.setTarget(img_logo);
+        animator.start();
     }
 }
